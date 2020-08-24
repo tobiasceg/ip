d94b141a71d0dddabae8e9999735b4bc0cd70002
tobiasceg <e0415553@u.nus.edu>
2020-08-24 23:09:46 +0800

    added the list bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String intro = " ____________________________________________________________\n" +
-                " Hello! I'm Duke the repeat bot\n" +
-                " What can I do for you?\n" +
-                " type \"bye\" after you are done having fun!\n" +
+                " Hello! I'm Duke the list bot\n" +
+                " Please list down your plans for today?\n" +
+                " type \"list\" to see the current list!\n" +
+                " type \"bye\" after you are done!\n" +
                 " ____________________________________________________________";
         System.out.println(intro);
 
         String reply;
         Scanner scan = new Scanner(System.in);
+
+        String[] list = new String[100];
+        int listCounter = 0;
 
         while(true) {
             reply = scan.nextLine();
@@ -23,9 +27,19 @@ public class Duke {
                         " ____________________________________________________________\n");
                 break;
             }
+            if(reply.equals("list")){
+                System.out.print(" ____________________________________________________________\n");
+                for ( int i = 1; i <= listCounter ; i++){
+                    System.out.println( i + ". " + list[i-1] );
+                }
+                System.out.println(" ____________________________________________________________\n");
+            }
             else {
+                list[listCounter] = reply;
+                listCounter++;
+
                 System.out.println(" ____________________________________________________________\n" +
-                        " " + reply + "\n" +
+                        " added: " + reply + "\n" +
                         " ____________________________________________________________");
             }
         }
