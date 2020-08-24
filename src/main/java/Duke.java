d23f591f29942b65764a06dd78ba4cafa33afb6b
tobiasceg <e0415553@u.nus.edu>
2020-08-24 22:23:55 +0800

    added repeat bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,33 @@
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         String intro = " ____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
+                " Hello! I'm Duke the repeat bot\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-
+                " type \"bye\" after you are done having fun!\n" +
+                " ____________________________________________________________";
         System.out.println(intro);
+
+        String reply;
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            reply = scan.nextLine();
+
+            if (reply.equals("bye")) {
+                System.out.println(" ____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        " ____________________________________________________________\n");
+                break;
+            }
+            else {
+                System.out.println(" ____________________________________________________________\n" +
+                        " " + reply + "\n" +
+                        " ____________________________________________________________");
+            }
+        }
     }
 }
