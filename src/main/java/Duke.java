3e1e0056c2d375ab750de00cc62cf4d1baea9c6c
tobiasceg <e0415553@u.nus.edu>
2020-08-31 15:22:01 +0800

    added Deadline,Event and Todo classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,13 @@ public class Duke {
 
         int listCounter = 0;
 
+        String taskName;
+        String dueDate;
+
+        int TODO_REMOVAL = 5;
+        int DEADLINE_REMOVAL = 9;
+        int EVENT_REMOVAL = 6;
+
         while (true) {
             reply = scan.nextLine();
 
@@ -27,9 +34,10 @@ public class Duke {
                         " ____________________________________________________________\n");
                 break;
             } else if (reply.equals("list")){
-                System.out.print(" ____________________________________________________________\n");
+                System.out.print(" ____________________________________________________________\n" +
+                        "Here are the tasks in your list:\n");
                 for ( int i = 1; i <= listCounter ; i++){
-                    System.out.println( i + ".[" + list[i-1].checkStatus() + "] " + list[i-1].description );
+                    System.out.println( i + "." + list[i-1].toString());
                 }
                 System.out.println(" ____________________________________________________________\n");
             } else if (reply.contains("done")){
@@ -38,15 +46,29 @@ public class Duke {
                         list[j-1].markAsDone(); // minus 1 coz the array of Task is 1 digit ahead
                     }
                 }
-            } else {
-                Task t = new Task(reply); //initialising the new task
-                list[listCounter] = t; //adding task to the list
-                listCounter++; //counter to keep track of number of tasks
-
-                System.out.println(" ____________________________________________________________\n" +
-                        " added: " + reply + "\n" +
-                        " ____________________________________________________________");
-            }
+            } else if (reply.contains("todo")){
+                taskName = reply.substring(TODO_REMOVAL);
+                Todo newTask = new Todo(taskName);
+                list[listCounter] = newTask; //added to the task list
+                listCounter++;
+                newTask.added(newTask); //output of the added message
+            } else if (reply.contains("deadline")){
+                taskName = reply.substring(DEADLINE_REMOVAL,reply.indexOf("/") - 1);
+                dueDate = reply.substring(reply.indexOf("/by") + 4);
+                Deadline newTask = new Deadline(taskName,dueDate);
+                list[listCounter] = newTask;
+                listCounter++;
+                newTask.added(newTask);
+            } else if (reply.contains("event")){
+                taskName = reply.substring(EVENT_REMOVAL,reply.indexOf("/") - 1);
+                dueDate = reply.substring(reply.indexOf("/at") + 4);
+                Event newTask = new Event(taskName,dueDate);
+                list[listCounter] = newTask;
+                listCounter++;
+                newTask.added(newTask);
+            } else System.out.println(" ____________________________________________________________\n" +
+                    "Missing Todo, Event or Deadline! \n" +
+                    " ____________________________________________________________");
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,13 +2,23 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    protected static int numberOfTasks = 0; //class-level member
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+
+        numberOfTasks++;
     }
 
     public String checkStatus() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    //going to be overloaded by subclasses
+    @Override
+    public String toString(){ //returning the task only with status box
+        return "[" + this.checkStatus() + "] " + this.description;
     }
 
     public void markAsDone(){
@@ -22,5 +32,13 @@ public class Task {
             System.out.println(" You have already finished the task!");
         }
         System.out.println(" ____________________________________________________________");
+    }
+
+    public void added(Task addedTask){
+        System.out.println(" ____________________________________________________________\n" +
+                "Got it. I've added this task:\n" +
+                 addedTask.toString() + "\n" +
+                "Now you have " + numberOfTasks + " tasks in the list\n" +
+                " ____________________________________________________________");
     }
 }
